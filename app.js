e15aba3cf22f2989ab3331b4fd6529f1f83a3585
Alpavlenko <al.pavlienko@it-justice.com>
2022-08-22 18:09:53 +0300

    init 3 part lessons

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const bodyParer = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const authRoutes = require('./routes/auth')
+const categoryRoutes = require('./routes/category')
+const orderRoutes = require('./routes/order')
+const positionRoutes = require('./routes/position')
+const analyticsRoutes = require('./routes/analytics')
+const keys = require('./config/keys')
+const app = express()
+
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('MD connected!'))
+    .catch((error) => console.log(error))
+
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
+
+app.use(morgan('dev'))
+app.use(bodyParer.urlencoded({extended: true}))
+app.use(bodyParer.json())
+app.use(cors())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/category', categoryRoutes)
+app.use('/api/order', orderRoutes)
+app.use('/api/position', positionRoutes)
+app.use('/api/analytics', analyticsRoutes)
+
+module.exports = app
---
+++ controllers/position.js
@@ -0,0 +1,35 @@
+const Position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.getByCategoryId = async function (req,res) {
+    try {
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        })
+        res.status(200).json(positions)
+    } catch (e) {
+        errorHandler(res, e)
+    }
+}
+module.exports.create = async function (req,res) {
+    try {
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
+}
+module.exports.remove = async function (req,res) {
+    try {
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
+}
+module.exports.update = async function (req,res) {
+    try {
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
+}
